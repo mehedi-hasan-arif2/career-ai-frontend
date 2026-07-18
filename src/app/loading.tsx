1a2e4637712242a5e0b81f6ace9ccaf367c05889
Mehedi Hasan Arif <iam.mehediarif@gmail.com>
2026-07-18 22:47:21 +0600

    complete Tailwind setup

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-[60vh] flex items-center justify-center">
-      <div className="h-10 w-10 border-2 border-primary border-t-transparent rounded-full animate-spin" />
-    </div>
-  );
-}
